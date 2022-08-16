85d07deaaf56abb4882a43f83ac6ec4627c15118
Colton Donkersgoed <coltonmdonk@gmail.com>
2022-08-16 17:19:18 -0400

    -- longest substring WIP

---
+++ length of longest substring.py
@@ -0,0 +1,77 @@
+def lengthOfLongestSubstring(s):
+    """
+    :type s: str
+    :rtype: int
+    """
+
+    len_substring = 0
+    current_highest = 0
+    un_unique = []
+
+    for char in s:
+
+        # traverse until non-unique char
+        if char not in un_unique:
+            len_substring += 1
+            un_unique.append(char)
+        # once we have a non-unique char, we set this as a substring
+        # and continue traversing the string
+        else:
+            if len_substring > current_highest:
+                current_highest = len_substring
+            un_unique = [char]
+
+            # reset the un_unique counter and continue from this letter...
+            len_substring = 1
+
+    # return the highest substring
+
+    if current_highest < len_substring:
+        current_highest = len_substring
+
+    return current_highest
+
+print(lengthOfLongestSubstring("dvdf"))
+
+def alt_lengthOfLongestSubstring(s):
+    """
+    :type s: str
+    :rtype: int
+    """
+
+    len_substring = 0
+    current_highest = 0
+    un_unique = []
+    counter = 0
+    start_point = 0
+
+    while start_point < len(s) - 1:
+
+        char = s[counter]
+        # traverse until non-unique char
+        if char not in un_unique:
+            len_substring += 1
+            un_unique.append(char)
+            counter += 1
+        # once we have a non-unique char, we set this as a substring
+        # and continue traversing the string
+        else:
+            if len_substring > current_highest:
+                current_highest = len_substring
+            un_unique = [char]
+
+            # reset the un_unique counter and continue from this letter..
+            len_substring = 1
+            start_point += 1
+            counter = start_point
+
+    # return the highest substring
+
+    if current_highest < len_substring:
+        current_highest = len_substring
+
+    return current_highest
+
+print(alt_lengthOfLongestSubstring("dvdf"))
+print(alt_lengthOfLongestSubstring("aab"))
+print(alt_lengthOfLongestSubstring("abcaabc"))
