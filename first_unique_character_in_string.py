e26dc46c056ca8a5b4d8da79a0e0c1cbbd452530
Colton Donkersgoed <coltonmdonk@gmail.com>
2022-08-15 22:04:25 -0400

    -- added alt solution using dict

--- first_unique_character_in_string.py
+++ first_unique_character_in_string.py
@@ -1,3 +1,5 @@
+
+
 class Solution(object):
     def firstUniqChar(self, s):
         """
@@ -23,3 +25,21 @@ class Solution(object):
             return s.index(unique[0])
         else:
             return -1
+
+
+class Alt_sol(object):
+    def firstUniqChar(self, s: str) -> int:
+        l_dict = dict()
+        uniquel_dict = dict()
+        for l in s:
+            if l not in l_dict.keys() and l not in uniquel_dict.keys():
+                l_dict.update({l: 1})
+                uniquel_dict.update({l: 1})
+            elif l in uniquel_dict.keys() and l in l_dict.keys():
+                l_dict.update({l: l_dict[l] + 1})
+                del uniquel_dict[l]
+            else:
+                l_dict.update({l: l_dict[l] + 1})
+        if uniquel_dict:
+            return s.index(list(uniquel_dict.keys())[0])
+        return -1
