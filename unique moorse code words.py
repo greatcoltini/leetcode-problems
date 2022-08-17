f28774e742ea0756a718b769ba2101b9e52c5e48
Colton Donkersgoed <coltonmdonk@gmail.com>
2022-08-17 16:56:03 -0400

    -- some spacing fixes

--- unique moorse code words.py
+++ unique moorse code words.py
@@ -12,12 +12,19 @@ class Solution(object):
                        "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."]
         transformations = []
         moorse_transformation = ""
+
         # add transformation to a list
         for word in words:
+
+            # for each letter in the word, convert it to it's moorse code representation
             for letter in word:
                 moorse_transformation += (moorse_code[string.lowercase.index(letter)])
+
             # check if item is a unique transformation
             if moorse_transformation not in transformations:
                 transformations.append(moorse_transformation)
+
+            # reset the transformation
             moorse_transformation = ""
+
         return len(transformations)
