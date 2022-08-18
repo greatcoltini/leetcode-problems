b3e8edc31e00d76fdb3269d6c7f6e881f52e1905
Colton Donkersgoed <coltonmdonk@gmail.com>
2022-08-18 17:46:38 -0400

    Update reduce array size to the half.py
    -- time limit exceeded, so find faster way

--- reduce array size to the half.py
+++ reduce array size to the half.py
@@ -7,21 +7,22 @@ class Solution(object):
 
         # initialization of the required variables
         integer_occurrences = {}
-        array_len = len(arr)
-        removed_num = 0
+        removed_num_integers = 0
         min_set = 0
 
-        # loop through array, create dict storing occurrences
-        for item in arr:
-            if item in integer_occurrences:
-                integer_occurrences[item] += 1
+        # sort array first
+        sorted_arr = reversed(sorted(arr))
+        for count, value in enumerate(sorted_arr):
+            if value in integer_occurrences:
+                integer_occurrences[value] += 1
             else:
-                integer_occurrences[item] = 1
+                integer_occurrences[value] = 1
+            count += 1
 
         # while our removed integers value is less than half the array, we continue to parse dict
-        while removed_num <= (array_len / 2):
+        while removed_num_integers < (count // 2):
             highest_occ_int = max(integer_occurrences, key=integer_occurrences.get)
-            removed_num = removed_num + max(integer_occurrences, key=integer_occurrences.get)
+            removed_num_integers = removed_num_integers + integer_occurrences[highest_occ_int]
             del integer_occurrences[highest_occ_int]
             min_set += 1
 
@@ -32,8 +33,12 @@ print(Solution().minSetSize([3, 3]))
 
 print(Solution().minSetSize([3, 3, 4, 4]))
 
-Solution().minSetSize([3, 3, 5, 6])
+#Solution().minSetSize([3, 3, 5, 6])
 
-Solution().minSetSize([3, 3, 7, 8, 9, 10])
+print(Solution().minSetSize([3,3,3,3,5,5,5,2,2,7]))
 
-print(Solution().minSetSize([1, 2, 3, 4, 5, 6]))
+print(Solution().minSetSize([10, 10, 9, 1]))
+
+
+#def minSetSize(self, arr: List[int]) -> int:
+#    return next(i+1 for i,n in enumerate(accumulate(reversed(sorted(Counter(arr).values())))) if n >= len(arr)//2)
