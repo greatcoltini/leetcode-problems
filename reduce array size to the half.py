e54e20429d023d99ecfbdd076806a7d779960a7d
Colton Donkersgoed <coltonmdonk@gmail.com>
2022-08-18 09:55:38 -0400

    -- updated soln for problem 1338

--- reduce array size to the half.py
+++ reduce array size to the half.py
@@ -4,26 +4,36 @@ class Solution(object):
         :type arr: List[int]
         :rtype: int
         """
-        integer_occurences = {}
+
+        # initialization of the required variables
+        integer_occurrences = {}
         array_len = len(arr)
         removed_num = 0
         min_set = 0
 
-        # loop through array, create dict storing occurences
+        # loop through array, create dict storing occurrences
         for item in arr:
-            if item in integer_occurences:
-                integer_occurences[item] += 1
+            if item in integer_occurrences:
+                integer_occurrences[item] += 1
             else:
-                integer_occurences[item] = 1
+                integer_occurrences[item] = 1
 
-        while removed_num <= (array_len // 2):
-            highest_occ_int = max(integer_occurences, key=integer_occurences.get)
-            removed_num = removed_num + max(integer_occurences, key=integer_occurences.get)
-            del integer_occurences[highest_occ_int]
+        # while our removed integers value is less than half the array, we continue to parse dict
+        while removed_num <= (array_len / 2):
+            highest_occ_int = max(integer_occurrences, key=integer_occurrences.get)
+            removed_num = removed_num + max(integer_occurrences, key=integer_occurrences.get)
+            del integer_occurrences[highest_occ_int]
             min_set += 1
-            print(removed_num)
 
-        print(integer_occurences)
-        print(min_set)
+        # return the minimum number of integers to be removed to make the array <= half of its initial size
+        return min_set
 
-Solution().minSetSize([3, 3])
+print(Solution().minSetSize([3, 3]))
+
+print(Solution().minSetSize([3, 3, 4, 4]))
+
+Solution().minSetSize([3, 3, 5, 6])
+
+Solution().minSetSize([3, 3, 7, 8, 9, 10])
+
+print(Solution().minSetSize([1, 2, 3, 4, 5, 6]))
