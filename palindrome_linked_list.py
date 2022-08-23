b49ecab6573c652caa7b74a84858798af2995236
Colton Donkersgoed <coltonmdonk@gmail.com>
2022-08-23 16:57:28 -0400

    -- commenting for the program
    -- added comments for the program

--- palindrome_linked_list.py
+++ palindrome_linked_list.py
@@ -1,10 +1,8 @@
 # Definition for singly-linked list.
-class ListNode(object):
-     def __init__(self, val=0, next=None):
-         self.val = val
-         self.next = next
-
-
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution(object):
 
     def isPalindrome(self, head):
@@ -12,14 +10,17 @@ class Solution(object):
         :type head: ListNode
         :rtype: bool
         """
+        # initialize variables
         count = 0
         list_items = []
+        # parse through the linked list
         while head:
             count += 1
             list_items.append(head.val)
             head = head.next
         reverse = reversed(list_items)
 
+        # enumerate the reversed list; if we hit non palindrome gg
         for count, value in enumerate(reverse):
             if not value == list_items[count]:
                 print(value)
@@ -27,7 +28,6 @@ class Solution(object):
                 return False
 
         return True
-
 
 palindrome = ListNode(3, ListNode(2, ListNode(3, None)))
 
