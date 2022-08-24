22e7efba1c650b5b033b2f9be89ccf92cf04a99c
Colton Donkersgoed <coltonmdonk@gmail.com>
2022-08-24 08:22:29 -0400

    alternatively, matching with regex

--- power of three.py
+++ power of three.py
@@ -1,18 +1,13 @@
+import numpy as np
+import re
+
 class Solution(object):
     def isPowerOfThree(self, n):
         """
         :type n: int
         :rtype: bool
         """
-         # change to float so we can use decimal values
-        i = float(n)
+        # match with regex
+        base_three = np.base_repr(n, 3)
+        return re.match(("^10*$"), base_three)
 
-        # loop through until we reach n value less than 4
-        while i >= 3:
-            i = i / 3
-
-        # if we have 1 we know it is divisible, else not
-        if i == 1:
-            return True
-        else:
-            return False
