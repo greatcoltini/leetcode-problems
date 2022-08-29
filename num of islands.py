780b0668fe0c928bfcbe513950bbf61935f860a8
Colton Donkersgoed <coltonmdonk@gmail.com>
2022-08-29 19:05:14 -0400

    better solution

--- num of islands.py
+++ num of islands.py
@@ -1,27 +1,25 @@
-class Solution(object):
-    def numIslands(self, grid):
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
 
-        def in_grid(x, y):
-            return 0 <= x < len(grid) and 0 <= y < len(grid[0])
+        # we are converting the grid to 0s because we have found a 1 connecting
+        def turn_to_dust(i, j):
+            if (i < 0 or j < 0 or i == len(grid) or j == len(grid[0]) or grid[i][j] == "0"):
+                return
+            grid[i][j] = "0"
+            turn_to_dust(i, j + 1)
+            turn_to_dust(i, j - 1)
+            turn_to_dust(i + 1, j)
+            turn_to_dust(i - 1, j)
+            return
 
-        def neighbours(node):
-            p, q = node
-            dir = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-            return [(x, y) for x, y in [(p + i, q + j) for i, j in dir] if in_grid(x, y)]
+        res = 0
 
-        def dfs(node):
-            visited.add(node)
-            for v in neighbours(node):
-                if grid[v[0]][v[1]]== "1" and v not in visited:
-                    dfs(v)
-
-        components = 0
-        visited = set()
+        # loop through the grid, if we encounter an island, kill all adjacent islands
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                node = (i, j)
-                if grid[i][j] == "1" and node not in visited:
-                    components += 1
-                    dfs(node)
+                if grid[i][j] == "1":
+                    res += 1
+                    turn_to_dust(i, j)
 
-        return components
+        
+        return res
