a9f70c3808026876f028632db915dac159ecf6bb
Colton Donkersgoed <coltonmdonk@gmail.com>
2023-02-27 21:55:09 -0500

    plus one soln

--- plus-one-66.py
+++ plus-one-66.py
@@ -16,9 +16,33 @@ class Solution(object):
         
         number_int += 1
         
-        numbers_str = str(numbers_int)
+        numbers_str = str(number_int)
         
         for char in numbers_str:
-            numbers_list.append(char)
+            numbers_list.append(int(char))
 
         return numbers_list
+    
+    
+# class Solution(object):
+    
+# def plusOne(digits):
+#         num_int = 0
+#         i = 1
+        
+#         for digit in digits:
+#             num_int += digit ** (len(digits) - i)
+#             i += 1
+            
+#         print(num_int)
+        
+#         num_int += 1
+#         digits_altered = []
+        
+#         while num_int > 0:
+#             digits_altered.append(num_int % 10)
+#             num_int = num_int // 10
+        
+#         return digits_altered
+
+# print(plusOne([1, 2, 3]))
