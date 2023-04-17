33b3e87846625c646185a383e436867939103ab0
Colton Donkersgoed <coltonmdonk@gmail.com>
2023-04-17 17:06:15 -0400

    greatest candies for kids

--- optimal-partition-of-string-2408.py
+++ optimal-partition-of-string-2408.py
@@ -7,17 +7,23 @@ def partitionString(s):
     # one approach
     # go char by char, creating new list of chars if char in list; for example
     #  "abbcccd" -> ["a"], ["ab"], ["ab", "b"], ["abc", "b"], ["abc", "bc"], ["abc", "bc", "c"], ["abcd", bc", "c"]
-    partition_string = [[]] * len(s)
+    partition_string = []
                 
     # try to remove s one at a time
     s = list(s)
     while s:
-        for string in partition_string:
-            if s[0] not in string:
-                string.append(s[0])
-                s = s[1:]
-                print(partition_string)
-                break
+        letter = s[0]
+        if partition_string:
+            for string in partition_string:
+                if letter not in string:
+                    string += letter
+                    letter = None
+                    s = s[1:]
+                    print(partition_string)
+                    break
+        else:
+            partition_string.append(letter)
+            s = s[1:]
                 
     # filter out empty strings
     partition_string = list(filter(None, partition_string))
@@ -25,4 +31,37 @@ def partitionString(s):
     print(partition_string)
     return len(partition_string)
 
-print(partitionString("abc"))
+def partitionStringV2(string):
+    # we want to take in string; repeat number of unique compositions
+    
+    #  approach: parse through string, and create list of lists
+    #  if character is in first list, we move to next, else, add it
+    #  return length of lists for number of unique lists created
+    partitioned_string = [[]] * 1000
+    entered = False
+    counter = 0 
+    for char in string:
+        counter = 0
+        entered = False
+        # loop through partitioned string until we have entered this char
+        while not entered:
+            if not partitioned_string[counter]:
+                partitioned_string[counter].append(char)
+                entered = True
+                
+            # if the partitioned string exists, look inside
+            if partitioned_string[counter]:
+                
+                if char not in partitioned_string[counter]:
+                    partitioned_string[counter].append(char)
+                    entered = True
+                    
+            # partitioned string does not exist, therefore add char
+            else:
+                partitioned_string[counter].append(char)
+                entered = True
+            counter += 1
+    return len(partitioned_string)                      
+        
+
+print(partitionStringV2("abcbc"))
